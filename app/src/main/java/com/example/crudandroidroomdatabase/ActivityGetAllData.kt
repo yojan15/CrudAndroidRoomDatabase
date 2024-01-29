7b19e3d280368f12ff8c55d3dd572a8436cab09d
yojan khuble <85030838+yojan15@users.noreply.github.com>
2024-01-29 15:27:37 +0530

    get data from database function added

---
+++ app/src/main/java/com/example/crudandroidroomdatabase/ActivityGetAllData.kt
@@ -0,0 +1,11 @@
+package com.example.crudandroidroomdatabase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityGetAllData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_all_data)
+    }
+}
---
+++ app/src/main/java/com/example/crudandroidroomdatabase/getData/TodosAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.crudandroidroomdatabase.getData
+
+class TodosAdapter {
+}
